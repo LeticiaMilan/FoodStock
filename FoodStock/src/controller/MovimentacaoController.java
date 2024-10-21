5e3eef16b2a035387186591bffaba20196d1dfdb
Letícia Milan <leticiamilan.dev@gmail.com>
2024-10-21 19:57:51 -0300

    feat: add estrutura mvc classes

--- FoodStock/src/controller/MovimentacaoController.java
+++ FoodStock/src/controller/MovimentacaoController.java
@@ -1,13 +1,11 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package controller;
 
-/**
- *
- * @author Leticia
- */
+import dao.MovimentacaoDAO;
+
 public class MovimentacaoController {
+    private MovimentacaoDAO movimentacaoDAO;
     
+    public MovimentacaoController() {
+        movimentacaoDAO = new MovimentacaoDAO();
+    }
 }
