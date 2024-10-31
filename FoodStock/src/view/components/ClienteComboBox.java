7294ddda52ad9c2c1235d168da38025431d6a084
Letícia Milan <leticiamilan.dev@gmail.com>
2024-10-31 01:11:20 -0300

    feat: cadastrar entrada de estoque

---
+++ FoodStock/src/view/components/ClienteComboBox.java
@@ -0,0 +1,32 @@
+package view.components;
+
+import controller.CategoriaController;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import javax.swing.JComboBox;
+import model.Categoria;
+
+public class CategoriaComboBox {
+    private Map<String, Integer> categoriaMap = new HashMap<>();
+    
+    public void carregarCategorias(JComboBox<String> comboBox) {
+        CategoriaController categoriaController = new CategoriaController();
+        ArrayList<Categoria> categorias = categoriaController.buscarCategorias();
+
+        comboBox.removeAllItems();
+        categoriaMap.clear();
+
+        for (Categoria categoria : categorias) {
+            String nome = categoria.getNome();
+            int id = categoria.getIdCategoria();
+            comboBox.addItem(nome);
+            categoriaMap.put(nome, id);
+        }
+    }
+    
+    public int getCategoriaId(JComboBox<String> comboBox) {
+        String nomeSelecionado = (String) comboBox.getSelectedItem();
+        return categoriaMap.get(nomeSelecionado);
+    }
+}
